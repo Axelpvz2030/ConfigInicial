129b274ac34330f593540359bb02519d6eac5fba
Axelpvz2030 <89265207+Axelpvz2030@users.noreply.github.com>
2025-05-08 22:43:11 -0600

    skybox

--- ConfigInicial/Texturizado.cpp
+++ ConfigInicial/Texturizado.cpp
@@ -185,7 +185,7 @@ int main()
 		DoMovement();
 
 		// Clear the colorbuffer
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		lampShader.Use();
